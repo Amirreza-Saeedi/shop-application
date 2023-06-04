c236bbd91044180b8af9abea025bedda7799404c
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-06-04 17:30:23 +0430

    project created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.example.shopapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.example.shopapplication to javafx.fxml;
+    exports com.example.shopapplication;
+}
