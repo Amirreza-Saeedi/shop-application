1cc6fc900c12b0471f76647dadd2473139327d34
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-07-04 17:41:31 +0430

    unfinished chat

--- src/main/java/com/example/shopapplication/ChatController.java
+++ src/main/java/com/example/shopapplication/ChatController.java
@@ -4,12 +4,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.*;
+import java.net.Socket;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -17,7 +21,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -38,11 +41,65 @@ public class ChatController implements Initializable {
     private TextArea messageTextArea;
     @FXML
     private Button sendButton;
+    @FXML
+    private Button exitButton;
 
-    private User from;
-    private User to;
-    private Admin admin;
-    private Seller seller;
+    static class FromAndTo {
+        private User from;
+        private User to;
+        private Admin admin;
+        private Seller seller;
+
+        @Override
+        public String toString() {
+            return "{from = " + from.getUsername() + ", to = " + to.getUsername() +
+                    ", admin = " + admin.getUsername() + ", seller = " + seller.getUsername() + "}";
+
+        }
+
+        public User getFrom() {
+            return from;
+        }
+
+        public void setFrom(User from) {
+            this.from = from;
+        }
+
+        public User getTo() {
+            return to;
+        }
+
+        public void setTo(User to) {
+            this.to = to;
+        }
+
+        public Admin getAdmin() {
+            return admin;
+        }
+
+        public void setAdmin(Admin admin) {
+            this.admin = admin;
+        }
+
+        public Seller getSeller() {
+            return seller;
+        }
+
+        public void setSeller(Seller seller) {
+            this.seller = seller;
+        }
+    }
+
+    FromAndTo fromAndTo = new FromAndTo();
+
+    // server-client
+    private Socket client;
+    private String host = "localhost";
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean done = false;
+
+
 
     private ObservableList<Message> messages = FXCollections.observableArrayList();
 
@@ -51,15 +108,16 @@ public class ChatController implements Initializable {
          * just 1 admin and 1 seller is accepted.
          * */
         // determine Admin & Seller
-        this.from = from;
-        this.to = to;
+
+        fromAndTo.setFrom(from);
+        fromAndTo.setTo(to);
 
         if (from instanceof Admin && to instanceof Seller) {
-            admin = (Admin) from;
-            seller = (Seller) to;
+            fromAndTo.setAdmin((Admin) from);
+            fromAndTo.setSeller((Seller) to);
         } else if (from instanceof Seller && to instanceof Admin) {
-            admin = (Admin) to;
-            seller = (Seller) from;
+            fromAndTo.setAdmin((Admin) to);
+            fromAndTo.setSeller((Seller) from);
         } else {
             throw new Exception("just 1 admin and 1 seller is accepted");
         }
@@ -67,54 +125,90 @@ public class ChatController implements Initializable {
         System.out.println("ChatController.ChatController");
         System.out.println("from = " + from);
         System.out.println("to = " + to);
-        System.out.println("seller = " + seller);
-        System.out.println("admin = " + admin);
+        System.out.println("seller = " + fromAndTo.getSeller());
+        System.out.println("admin = " + fromAndTo.getAdmin());
 
-        // auto-clear
+        // auto-clear chats
         autoClear();
 
-        // load
+        // load chats
         load();
 
+//        connectToServer();
     }
 
-//    public ChatController(User from, User to) throws Exception {
-//        /**
-//         * just 1 admin and 1 seller is accepted.
-//         * */
-//        // determine Admin & Seller
-//        this.from = from;
-//        this.to = to;
-//
-//        if (from instanceof Admin && to instanceof Seller) {
-//            admin = (Admin) from;
-//            seller = (Seller) to;
-//        } else if (from instanceof Seller && to instanceof Admin) {
-//            admin = (Admin) to;
-//            seller = (Seller) from;
-//        } else {
-//            throw new Exception("just 1 admin and 1 seller is accepted");
-//        }
-//
-//        System.out.println("ChatController.ChatController");
-//        System.out.println("from = " + from);
-//        System.out.println("to = " + to);
-//        System.out.println("seller = " + seller);
-//        System.out.println("admin = " + admin);
-//
-//        // auto-clear
-//        autoClear();
-//
-//        // load
-//        load();
+//    private void connectToServer() throws IOException {
+//        // connect to server
+//        client = new Socket(host, ChatServer.port);
+//        System.out.println("connected to server.");
+//        out = new PrintWriter(client.getOutputStream(), true);
+//        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//        out.println("hello server");
 //
 //    }
 
     public ChatController() {
         System.out.println("ChatController.ChatController");
-        System.out.println("no args");
+        System.out.println("default constructor");
+
     }
 
+    public void close() {
+        System.out.println("ChatController.close");
+    }
+
+    private void shutDown() {
+        done = true;
+        try {
+            in.close();
+            out.close();
+            if (!client.isClosed()) {
+                client.close();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    @Override
+//    public void run() {
+//        try {
+//            client = new Socket(host, ChatServer.port); // connect to server
+//            System.out.println("client " + fromAndTo.getFrom() + " connected.");
+//            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//            out = new PrintWriter(client.getOutputStream(), true);
+//
+//            out.print(fromAndTo); // pass from-and-to to server
+////            in.read(); // temp
+//
+//            // handle in-messages
+//            String message;
+//            while (!done) {
+//
+//            }
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    public void send() {
+        String message = messageTextArea.getText();
+
+
+
+
+    }
+
+    public void exit() {
+        shutDown();
+
+
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+//        stage.getOnCloseRequest().handle(null);
+        stage.close();
+    }
 
     public static void main(String[] args) {
     }
@@ -145,7 +239,7 @@ public class ChatController implements Initializable {
             // read form Chats all seller's messages
             Statement statement = connection.createStatement();
             String sql = "SELECT * FROM Chats where " +
-                    "sellerId ='" + seller.getUsername() +"' ORDER BY date ASC, time ASC ";
+                    "sellerId ='" + fromAndTo.getSeller().getUsername() +"' ORDER BY date ASC, time ASC ";
             ResultSet resultSet = statement.executeQuery(sql);
 
             ArrayList<Message> list = new ArrayList<>();
@@ -200,4 +294,7 @@ public class ChatController implements Initializable {
             throw new RuntimeException(e);
         }
     }
+
+
+
 }
