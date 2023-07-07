ad868e796d9a63eca84c36664590e2d89560a025
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-07-07 17:02:07 +0430

    unfinished chart

--- src/main/java/com/example/shopapplication/StorageChartController.java
+++ src/main/java/com/example/shopapplication/StorageChartController.java
@@ -10,7 +10,15 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
 
+import java.net.JarURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -27,6 +35,9 @@ public class StorageChartController implements Initializable {
     private Storage storage;
     private ObservableList<String> list = FXCollections.observableArrayList();
     private ArrayList<Number> dataList = new ArrayList<>(30);
+    private XYChart.Data<String, Number>[] dataDaily = new XYChart.Data[30];
+    private XYChart.Data<String, Number>[] dataMonthly = new XYChart.Data[12];
+    private XYChart.Data<String, Number>[] dataYearly = new XYChart.Data[5];
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -60,8 +71,32 @@ public class StorageChartController implements Initializable {
         System.out.println("StorageChartController.loadDaily");
         series.setName("Daily Properties");
 
+        try (Connection connection = new DatabaseConnectionJDBC().getConnection()) {
+            Statement statement = connection.createStatement();
+            LocalDate localDate = LocalDate.now();
+            localDate = localDate.minusDays(29);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String date = localDate.format(formatter);
+//            String sql = "select * from StorageProperties where storageId='" + storage.getId() + "' and daily>='" + date +
+//                    "' order by daily asc";
+            String sql = "select * from StorageProperties where storageId='" + storage.getId() +
+                    "' order by daily asc LIMIT 30";
 
-        series.getData().setAll(dataList);
+            ResultSet resultSet = statement.executeQuery(sql);
+            for (int i = 0; resultSet.next()); ++i {
+                double value = resultSet.getDouble("value");
+                int amount = resultSet.getInt("amount");
+
+
+            }
+
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        series.getData().setAll(dataDaily);
     }
 
     private void loadMonthly() {
